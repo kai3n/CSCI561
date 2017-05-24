67ca1c1eeda14e3f55909777c93c4fd0e75ecae1
Mingun Pak <mingunpa@usc.edu>
2017-05-23 18:20:47 -0700

    assignment

--- agent_goalbased/agent_goalbased.py
+++ agent_goalbased/agent_goalbased.py
@@ -1,5 +1,19 @@
 import sys
 
+"""
+    Team Member
+    Name: Mingun Pak            E-mail: mingunpa@usc.edu
+    Name: Ali Marjaninejad      E-mail: marjanin@usc.edu
+    Name: Qixiong Liu           E-mail: qixiongl@usc.edu
+    Name: Chengyu Shen          E-mail: shenchen@usc.edu
+
+    Agent Description
+    This is the zombie killer agent. This environment contains two rooms, which are A and B. A is in the left hand side of B.
+    If there is a zombie (or more) inside the current room, we need to shoot and kill all the zombie inside this room and move the another then.
+    If there is no zombie, we should move to another room.
+    Goal-based: Goal describes what agent wants. Our goal for this task is make both rooms are clean. So if both of them are clean, we terminate the program.
+
+"""
 lines = []
 intline = []
 model = {'A': None, 'B': None}
