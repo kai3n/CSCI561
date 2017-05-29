ca7b715adcb785a1ac5b75aeac3bafd1ec0065f0
Mingun Pak <mingunpa@usc.edu>
2017-05-28 22:10:11 -0700

    add: bfs, dfs

--- hw1/main.py
+++ hw1/main.py
@@ -7,11 +7,56 @@ print("method: {}".format(method))
 print("fuel: {}".format(fuel))
 print("s: {}".format(s))
 print("d: {}".format(d))
+char_dic = {}
+index = 0
 for node_line in input_list[4:]:
     node_line = node_line.replace(":", "").replace(",", "").split()
     target_node = [node_line[0]]
+    char_dic[node_line[0]] = index
+    index += 1
     for i in range(1, len(node_line)):
         target_node.append(node_line[i].split("-"))
     graph.append(target_node)
 
 print(graph)
+print(char_dic)
+# def bfs(graph, s, d):
+#     queue = [(s, [s], 0)]
+#     while queue:
+#         (vertex, path, weight) = queue.pop(0)
+#         for n, w in graph[char_dic[vertex]][1:]:
+#             if n not in set(path):
+#                 if int(fuel) >= (weight + int(w)):
+#                     if n == d:
+#                         return '-'.join(path + [n]) + " " + str(int(fuel) - (weight + int(w)))
+#                     else:
+#                         queue.append((n, path + [n], weight + int(w)))
+
+def bfs(graph, s, d):
+    queue = [(s, [], 0)]
+    while queue:
+        (vertex, path, weight) = queue.pop(0)
+        if vertex not in set(path):
+            if int(fuel) >= weight:
+                if vertex == d:
+                    return '-'.join(path + [vertex]) + " " + str(int(fuel) - weight)
+                else:
+                    for n, w in graph[char_dic[vertex]][1:]:
+                        queue.append((n, path + [vertex], weight + int(w)))
+    return "No Path"
+
+def dfs(graph, s, d):
+    stack = [(s, [], 0)]
+    while stack:
+        (vertex, path, weight) = stack.pop()
+        if vertex not in set(path):
+            if int(fuel) >= weight:
+                if vertex == d:
+                    return '-'.join(path + [vertex]) + " " + str(int(fuel) - weight)
+                else:
+                    for n, w in graph[char_dic[vertex]][1:][::-1]:
+                        stack.append((n, path + [vertex], weight + int(w)))
+    return "No Path"
+
+print(bfs(graph, s, d))
+# print(dfs(graph, s, d))
